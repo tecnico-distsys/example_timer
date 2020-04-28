bce912614923987426cb60b9397eece6eb6d64b2
Miguel Pardal <miguel.pardal@tecnico.ulisboa.pt>
2020-04-28 23:44:43 +0100

    Add code

---
+++ src/main/java/pt/tecnico/sdis/timer/MyTimerTask.java
@@ -0,0 +1,25 @@
+package pt.tecnico.sdis.timer;
+
+import java.util.TimerTask;
+
+public class MyTimerTask extends TimerTask {
+
+    Object argument;
+
+    public MyTimerTask(Object argument) {
+        this.argument = argument;
+    }
+
+    @Override
+    public void run() {
+        System.out.println(this.getClass() + " running...");
+
+        // read argument (any object)
+        Integer arg = (Integer) this.argument;
+
+        // Do something...
+        System.out.println(arg);
+
+    }
+
+}
